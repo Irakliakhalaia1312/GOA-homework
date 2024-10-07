42a81652b127eb6f64fab55298dedf51c4b3ba52
Irakliakhalaia1312 <irakliaxalaia1212@gmail.com>
2024-10-07 18:36:20 +0200

    first commit

---
+++ dqy 1/lesson1.py
@@ -0,0 +1,55 @@
+from turtle import*
+
+
+
+#we want to point a house
+
+#step 1:  draw a square
+
+speed(10)
+width(7)
+color("green")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of squeare
+
+#draving a door
+
+forward(70)
+color("yellow")
+left(90)
+forward(120)   #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+
+
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+
+
+
+exitonclick()
+print("day 0 homework")
---
+++ me/lesson1.py
@@ -0,0 +1,11 @@
+name = "Irakli"
+
+
+surname = "akhalaia"
+age = 20 
+height = 170
+knows_proggraming = False
+
+
+
+print(name+surname,"age" + str(20),"height" + str(170),"knows_proggraming" + str(False) )
